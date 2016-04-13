9c6e11c4cbc02b4028cb0190262d35f1867eef05
Apple <Apple@192.168.1.71>
2016-04-13 17:58:31 +0800

    test

---
+++ src/test/java/tw/com/softleader/eeit8600/music/service/MusicServiceTest.java
@@ -0,0 +1,31 @@
+package tw.com.softleader.eeit8600.music.service;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import tw.com.softleader.eeit8600.music.entity.MusicEntity;
+
+public class MusicServiceTest {
+
+	@Test
+	public void testGetById() {
+		MusicService musicService = new MusicService();
+		
+		MusicEntity music = musicService.getById(1);
+		
+		assertEquals("Beethoven", music.getMuauthor());
+		assertEquals(500, music.getMuprice().intValue());
+	}
+	
+	@Test
+	public void testGetAll() {
+		MusicService musicService = new MusicService();
+		List<MusicEntity> musics = musicService.getAll();
+		
+		assertEquals(2, musics.size());
+	}
+
+}
