7f41a5640e2482524c8a299a823c467b890e717a
Hsiang <lovejune413@hotmail.com>
2016-04-20 00:13:01 +0800

    11

---
+++ src/test/java/tw/com/softleader/eeit8600/notebook/service/NotebookServiceTest.java
@@ -0,0 +1,30 @@
+package tw.com.softleader.eeit8600.notebook.service;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import tw.com.softleader.eeit8600.notebook.entity.Notebook;
+import tw.com.softleader.eeit8600.notebook.service.NotebookService;
+
+public class NotebookServiceTest {
+
+	@Test
+	public void testGetById() {
+		NotebookService NotebookService= new NotebookService();
+		Notebook nb = NotebookService.getById(1234567);
+		
+		assertEquals(25000,nb.getPrice().intValue());
+	}
+
+	@Test
+	public void testGetAll() {
+		NotebookService  NotebookService=new NotebookService();
+		List<Notebook> nbs = NotebookService.getAll();
+		
+		assertEquals(2,nbs.size());
+	}
+
+}
