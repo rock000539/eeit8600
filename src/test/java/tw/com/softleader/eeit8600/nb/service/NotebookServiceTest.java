d364e98a67da27959bd482f5db2b2b2b67a2a7e0
Hsiang <lovejune413@hotmail.com>
2016-04-19 22:44:59 +0800

    1

--- src/test/java/tw/com/softleader/eeit8600/nb/service/NotebookServiceTest.java
+++ src/test/java/tw/com/softleader/eeit8600/nb/service/NotebookServiceTest.java
@@ -6,22 +6,23 @@ import java.util.List;
 
 import org.junit.Test;
 
-import tw.com.softleader.eeit8600.nb.entity.NotebookBean;
+import tw.com.softleader.eeit8600.notebook.entity.Notebook;
+import tw.com.softleader.eeit8600.notebook.service.NotebookService;
 
 public class NotebookServiceTest {
 
 	@Test
 	public void testGetById() {
 		NotebookService NotebookService= new NotebookService();
-		NotebookBean nb = NotebookService.getById(1234567);
+		Notebook nb = NotebookService.getById(1234567);
 		
-		assertEquals(25000,nb.getNbprice().intValue());
+		assertEquals(25000,nb.getPrice().intValue());
 	}
 
 	@Test
 	public void testGetAll() {
 		NotebookService  NotebookService=new NotebookService();
-		List<NotebookBean> nbs = NotebookService.getAll();
+		List<Notebook> nbs = NotebookService.getAll();
 		
 		assertEquals(2,nbs.size());
 	}
