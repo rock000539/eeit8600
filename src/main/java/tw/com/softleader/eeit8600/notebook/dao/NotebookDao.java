ccade47a9645f70e35938941d196d1e38cec3818
Hsiang <lovejune413@hotmail.com>
2016-04-20 00:12:03 +0800

    2

---
+++ src/main/java/tw/com/softleader/eeit8600/notebook/dao/NotebookDao.java
@@ -0,0 +1,238 @@
+package tw.com.softleader.eeit8600.notebook.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import tw.com.softleader.eeit8600.book.entity.Book;
+import tw.com.softleader.eeit8600.notebook.entity.Notebook;
+public class NotebookDao {
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		String jdbcDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"; 
+		String dbUrl = "jdbc:sqlserver://softleader.com.tw:1433;database=EEIT86DB";
+		try {
+			Class.forName(jdbcDriver);
+			conn = DriverManager.getConnection(dbUrl, "EEIT86", "EEIT86");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public Notebook findById(int id) {
+		String sqlcmd = "SELECT * FROM Notebook WHERE id=?";
+		Notebook notebook = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sqlcmd);
+			pstmt.setLong(1, id);
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				notebook = new Notebook();
+				notebook.setId(rs.getInt(1));
+				notebook.setBrand(rs.getString(2));
+				notebook.setName(rs.getString(3));
+				notebook.setCpu(rs.getString(4));
+				notebook.setPrice(rs.getInt(5));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+		}
+		
+		return notebook;
+	}//end of findById
+	
+	
+	public List<Notebook> findAll() {
+		String sqlCmd = "SELECT * FROM Notebook";
+		List<Notebook> notebooks = new ArrayList();
+		Notebook notebook = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sqlCmd);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				notebook = new Notebook();
+				notebook.setId(rs.getInt(1));
+				notebook.setBrand(rs.getString(2));
+				notebook.setName(rs.getString(3));
+				notebook.setCpu(rs.getString(4));
+				notebook.setPrice(rs.getInt(5));
+				notebooks.add(notebook);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+			if (conn!=null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+		}
+		return notebooks;
+	}//end of findAll
+	
+	public int insert(Notebook notebook) {
+		String sqlCmd = "INSERT INTO Notebook(ID, BRAND, NAME, CPU, PRICE)"
+				+ " VALUES ("+notebook.getId()+",'"+notebook.getBrand()+"','"+notebook.getName()+"','"+notebook.getCpu()+"','"+notebook.getPrice()+"')";
+		System.out.println(sqlCmd);
+		
+		int rs = 0;
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			
+			conn = getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeUpdate(sqlCmd);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally{
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+			
+		}
+		
+		return rs;
+	}//end of insert
+	
+	public int update(Notebook notebook){
+		String sqlCmd = "UPDATE Notebook SET "
+				+ "BRAND='"+notebook.getBrand()+"',"
+				+ "NAME='"+notebook.getName()+"',"
+				+ "CPU='"+notebook.getCpu()+"',"
+				+ "PRICE="+notebook.getPrice()+" "
+				+ "WHERE ID="+notebook.getId();
+		
+		System.out.println(sqlCmd);
+		
+		int rs = 0;
+		
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			conn = getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeUpdate(sqlCmd);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally{
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+			if (conn !=null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+		}
+		
+		return rs;
+	}
+
+	public int delete(int id) {
+		
+		String sqlCmd = "DELETE FROM Notebook WHERE ID="+id;
+		System.out.println(sqlCmd);
+		int rs = 0;
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			conn = getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeUpdate(sqlCmd);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally{
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+			if (conn !=null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+		}
+		
+		return rs;
+	}//end of delete
+	
+}//end of class
---
+++ src/test/java/tw/com/softleader/eeit8600/notebook/dao/NotebookDaoTest.java
@@ -0,0 +1,58 @@
+package tw.com.softleader.eeit8600.notebook.dao;
+
+import java.util.List;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import tw.com.softleader.eeit8600.book.entity.Book;
+import tw.com.softleader.eeit8600.notebook.entity.Notebook;
+
+public class NotebookDaoTest {
+
+	
+	
+	@Test
+	public void testCrud() {
+		NotebookDao notebookDao = new NotebookDao();
+		
+		// find all data
+		List<Notebook> notebooks = notebookDao.findAll();
+		int originalSize = notebooks.size();
+		
+		// test insert
+		Notebook nb1 = new Notebook();
+		nb1.setId(1);
+		nb1.setBrand("ASUS");
+		nb1.setName("ZenBook");
+		nb1.setCpu("Intel® Core™ M 5Y10 處理器");
+		nb1.setPrice(40000);
+		
+		NotebookDao.insert(nb1);
+		
+		notebooks = NotebookDao.findAll();
+		assertEquals(originalSize+1, notebooks.size());
+		
+		// test findById
+		Notebook dbNotebook = NotebookDao.findById(1);
+		assertEquals(nb1.getBrand(), dbNotebook.getBrand());
+		assertEquals(nb1.getName(), dbNotebook.getName());
+		assertEquals(nb1.getCpu(), dbNotebook.getCpu());
+		assertEquals(nb1.getPrice(), dbNotebook.getPrice());
+		
+		// test update
+		dbNotebook.setPrice(60000);
+		NotebookDao.update(dbNotebook);
+		
+		Book updatedBook = NotebookDao.findById(1);
+		assertEquals(dbNotebook.getPrice(), updatedBook.getPrice());
+		
+		// test delete
+		NotebookDao.delete(1L);
+		notebooks = NotebookDao.findAll();
+		
+		assertEquals(originalSize, notebooks.size());
+		
+	}
+
+}
