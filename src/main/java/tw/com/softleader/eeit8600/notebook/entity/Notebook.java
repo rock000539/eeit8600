32b29e4d9faa84c348fbb357a82b80ba8056e448
Hsiang <lovejune413@hotmail.com>
2016-04-19 22:40:50 +0800

    1

---
+++ src/main/java/tw/com/softleader/eeit8600/notebook/entity/Notebook.java
@@ -0,0 +1,56 @@
+package tw.com.softleader.eeit8600.notebook.entity;
+
+public class Notebook {
+	
+	private int        id;
+	private String  brand;
+	private String   name;
+	private String    cpu;
+	private Integer price;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpu() {
+		return cpu;
+	}
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	
+	
+		
+
+
+	
+
+
+	
+	
+	
+
+	  
+}
