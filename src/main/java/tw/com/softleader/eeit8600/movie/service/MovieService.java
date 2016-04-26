8529ebb9567e1b1ed6e376c9111246584cdabc40
katesong <katess24@gmail.com>
2016-04-26 12:19:28 +0800

    movieService and movieList

--- src/main/java/tw/com/softleader/eeit8600/movie/service/MovieService.java
+++ src/main/java/tw/com/softleader/eeit8600/movie/service/MovieService.java
@@ -4,54 +4,37 @@ package tw.com.softleader.eeit8600.movie.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import tw.com.softleader.eeit8600.book.dao.BookDao;
+import tw.com.softleader.eeit8600.book.entity.Book;
+import tw.com.softleader.eeit8600.movie.dao.MovieDao;
 import tw.com.softleader.eeit8600.movie.entity.Movie;
 
 public class MovieService {
-
-	public static void main(String[] args) {
-		Movie movie = new MovieService().getById(1);
-		System.out.println(movie);
-		
-		List<Movie> movies = new MovieService().getAll();
-		for(Movie m: movies) {
-			System.out.println(m);
-		}
+	
+	private MovieDao movieDao;
+	
+	public MovieService() {
+		movieDao = new MovieDao();
 	}
-
+	
 	public Movie getById(int id) {
-		
-		
-		Movie movie1 = new Movie();
-		movie1.setId(1);
-		movie1.setName("Batman Begin");
-		movie1.setActor("Christian Bale");
-		movie1.setTypes("Action");
-		
-		return movie1;
+		return movieDao.findById(id);		
 	}
-
-
+	
 	public List<Movie> getAll() {
-		
-		List<Movie> movies = new ArrayList<Movie>();
-		
-		Movie movie1 = new Movie();
-		movie1.setId(1);
-		movie1.setName("Bat Man");
-		movie1.setActor("Ben");
-		movie1.setTypes("Actor");
-
-		movies.add(movie1);
-		
-		Movie movie2 = new Movie();
-		movie2.setId(2);
-		movie2.setName("Harry Porter");
-		movie2.setActor("Harry");
-		movie2.setTypes("fiction");
-		
-		movies.add(movie2);
-		
-		return movies;
+		return movieDao.findAll();
+	}
+	
+	public void insert(Movie movie) {
+		movieDao.insert(movie);
+	}
+	
+	public void update(Movie movie) {
+		movieDao.update(movie);
+	}
+	
+	public void delete(int id) {
+		movieDao.delete(id);
 	}
 	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,4 +5,5 @@ Hello, This is EEIT86 TestWebSite
 	<li><a href="${pageContext.request.contextPath}/book/bookList.jsp">Book Management</a></li>
 	<li><a href="${pageContext.request.contextPath}/app/AppList.jsp">App Management</a></li>
 	<li><a href="${pageContext.request.contextPath}/coffee/coffeeList.jsp">Coffee Management</a></li>
+	<li><a href="${pageContext.request.contextPath}/movie/movieList.jsp">Movie Management</a></li>
 </ul>
