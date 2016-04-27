6f958241ef74c2d2e6b50007b67fb1efd81de806
teyushen <teyushen@gmail.com>
2016-04-27 17:23:31 +0800

    controller

---
+++ src/main/java/tw/com/softleader/eeit8600/drama/web/DramaController.java
@@ -0,0 +1,22 @@
+package tw.com.softleader.eeit8600.drama.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import tw.com.softleader.eeit8600.drama.service.DramaService;
+
+@Controller
+@RequestMapping("/drama")
+public class DramaController {
+	@Autowired
+	private DramaService dramaService;
+	
+	@RequestMapping("/list")
+	public String listPage(Model model){
+		model.addAttribute("dramas",dramaService.getAll());
+		return "/drama/dramaList";
+	}
+
+}
