fa45c7b36ad24436b4ca3fbe616ddffa47ee85d8
Parker <rock000539@gmail.com>
2016-04-27 17:10:14 +0800

    Created coffeeController.java

---
+++ src/main/java/tw/com/softleader/eeit8600/coffee/web/CoffeeController.java
@@ -0,0 +1,22 @@
+package tw.com.softleader.eeit8600.coffee.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import tw.com.softleader.eeit8600.book.service.BookService;
+
+@Controller
+@RequestMapping("/coffees")
+public class CoffeeController {
+	@Autowired
+	private BookService coffeeService;
+
+	@RequestMapping("/list")
+	public String listPage(Model model) {
+		model.addAttribute("coffees", coffeeService.getAll());
+		return "/coffee/coffeeList";
+	}
+
+}
