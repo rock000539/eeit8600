b5be0db2618bc67fb31cc2a24b09fce813caa001
Hsiang <lovejune413@hotmail.com>
2016-05-02 22:31:44 +0800

    add entity

--- src/main/java/tw/com/softleader/eeit8600/notebook/entity/Notebook.java
+++ src/main/java/tw/com/softleader/eeit8600/notebook/entity/Notebook.java
@@ -1,56 +1,85 @@
 package tw.com.softleader.eeit8600.notebook.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+ private int        id;
+ private String  brand;
+ private String   name;
+ private String    cpu;
+ private Integer price;
+ */
+
+@Entity
+@Table(name = "NOTEBOOK")
 public class Notebook {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	private int id;
 	
-	private int        id;
-	private String  brand;
-	private String   name;
-	private String    cpu;
+	@Column(name="BRAND", length=50)
+	private String brand;
+	
+	@Column(name="NAME", length=50)
+	private String name;
+	
+	@Column(name="CPU", length=50)
+	private String cpu;
+	
+	@Column(name="PRICE")
 	private Integer price;
 	
 	
+	@Override
+	public String toString() {
+		return "Notebook [id=" + id + ", brand=" + brand + ", name=" + name
+				+ ", cpu=" + cpu + ", price=" + price + "]";
+	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getBrand() {
 		return brand;
 	}
+
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCpu() {
 		return cpu;
 	}
+
 	public void setCpu(String cpu) {
 		this.cpu = cpu;
 	}
+
 	public Integer getPrice() {
 		return price;
 	}
+
 	public void setPrice(Integer price) {
 		this.price = price;
 	}
-
-	
-	
-		
-
-
-	
-
-
-	
-	
-	
-
-	  
 }
