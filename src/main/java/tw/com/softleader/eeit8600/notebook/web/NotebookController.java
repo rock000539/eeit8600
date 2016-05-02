08b734b8df5a3e647e48eab016b5e238c778a1e1
Hsiang <lovejune413@hotmail.com>
2016-05-02 23:22:12 +0800

    update controller

--- src/main/java/tw/com/softleader/eeit8600/notebook/web/NotebookController.java
+++ src/main/java/tw/com/softleader/eeit8600/notebook/web/NotebookController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import tw.com.softleader.eeit8600.notebook.entity.Notebook;
 import tw.com.softleader.eeit8600.notebook.service.NotebookService;
 
 @Controller
@@ -22,8 +23,20 @@ public class NotebookController {
 			//"WEB-INF/jsp/notebook/notebookList.jsp"
 			
 			model.addAttribute("notebooks", notebookService.getAll());
-		
 			return "/notebook/notebookList";
-	
+		}
+		
+		@RequestMapping(value="/edit")
+		public String editPage(Long id,Model model){
+			model.addAttribute("notebook", notebookService.getById(id));
+			return "/notebook/notebookEdit";
+			
+		}
+		
+		@RequestMapping(value="delete")
+		public String delete(Long id,Model model){
+			notebookService.delete(id);
+			return "redirect:/notebook/list";
+			
 		}
 }
