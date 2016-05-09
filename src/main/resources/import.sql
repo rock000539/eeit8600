bf20a6d5f50afd57a4914f9cd70853f06fdf90ee
Choco0913 <purplemaple0913@gmail.com>
2016-05-09 16:44:32 +0800

    update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO DRAMA (NAME, EPISODES, ACTOR, GENRE) VALUES ('The Walking Dead', 16,  'Andrew Lincoln', 'Horror' );
 INSERT INTO MOVIE (NAME, ACTOR, GENRE, RATING) VALUES ('Minions', 'Sandra Bullock', 'Animation', 6.5);
 INSERT INTO COFFEE (NAME, LOCAL, PRICE, TESTING) VALUES ('first', 'U.S', 200, 'yes');
+INSERT INTO GAME (NAME, EVALUATION, DOWNLOAD, URL) VALUES ('部落衝突', 5, 3000000, 'www.coc.com.tw');
