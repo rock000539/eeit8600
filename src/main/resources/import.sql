d7e45aec29b9bd4b8ce2248ea65e44b2d5d674f3
katesong <katess24@gmail.com>
2016-05-09 16:40:59 +0800

    coffee import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 INSERT INTO DRAMA (NAME, EPISODES, ACTOR, GENRE) VALUES ('The Walking Dead', 16,  'Andrew Lincoln', 'Horror' );
 INSERT INTO MOVIE (NAME, ACTOR, GENRE, RATING) VALUES ('Minions', 'Sandra Bullock', 'Animation', 6.5);
+INSERT INTO COFFEE (NAME, LOCAL, PRICE, TESTING) VALUES ('first', 'U.S', 200, 'yes');
