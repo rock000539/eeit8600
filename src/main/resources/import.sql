e205f4d4fcb81982045f8d3e167a898534844f6d
katesong <katess24@gmail.com>
2016-05-09 16:38:41 +0800

    import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
 INSERT INTO DRAMA (NAME, EPISODES, ACTOR, GENRE) VALUES ('The Walking Dead', 16,  'Andrew Lincoln', 'Horror' );
+INSERT INTO MOVIE (NAME, ACTOR, GENRE, RATING) VALUES ('Minions', 'Sandra Bullock', 'Animation', 6.5);
