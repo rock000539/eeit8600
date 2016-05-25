fdb0cdc39c7cf96a6e825db83a66a212d734f4b5
Gary Lee <jakarta99@gmail.com>
2016-05-25 19:12:41 +0800

    ajax sayHello sample

--- src/main/java/tw/com/softleader/eeit8600/book/web/BookController.java
+++ src/main/java/tw/com/softleader/eeit8600/book/web/BookController.java
@@ -3,8 +3,11 @@ package tw.com.softleader.eeit8600.book.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import tw.com.softleader.eeit8600.book.entity.Book;
 import tw.com.softleader.eeit8600.book.service.BookService;
 
 @Controller
@@ -24,5 +27,30 @@ public class BookController {
 		
 		return "/book/bookList"; 
 	}
+	
+	
+	
+	@RequestMapping(value="/sayHello")
+	@ResponseBody
+	public Book sayHello(@RequestBody Book book) {
+		
+		return book;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
