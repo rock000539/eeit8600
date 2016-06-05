284f7308cc590773175d4d9d75fa129ace07b146
Parker <rock000539@gmail.com>
2016-06-05 16:56:14 +0800

    Table date 20% already finish

---
+++ src/main/java/tw/com/softleader/eeit8600/product/entity/Date.java
@@ -0,0 +1,87 @@
+package tw.com.softleader.eeit8600.product.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DATE")
+public class Date {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "dateId")
+	private Long dateId;
+	
+	@Column(name = "memberId")
+	private Long memberId;
+	
+	@Column(name = "proId")
+	private Long proId;
+	
+	@Column(name = "batchCode")
+	private Integer batchCode;
+	
+	@Column(name = "mfd")
+	private java.sql.Date mfd;
+	
+	@Column(name = "exp")
+	private java.sql.Date exp;
+
+	public Long getDateId() {
+		return dateId;
+	}
+
+	public void setDateId(Long dateId) {
+		this.dateId = dateId;
+	}
+
+	public Long getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+	}
+
+	public Long getProId() {
+		return proId;
+	}
+
+	public void setProId(Long proId) {
+		this.proId = proId;
+	}
+
+	public Integer getBatchCode() {
+		return batchCode;
+	}
+
+	public void setBatchCode(Integer batchCode) {
+		this.batchCode = batchCode;
+	}
+
+	public java.sql.Date getMfd() {
+		return mfd;
+	}
+
+	public void setMfd(java.sql.Date mfd) {
+		this.mfd = mfd;
+	}
+
+	public java.sql.Date getExp() {
+		return exp;
+	}
+
+	public void setExp(java.sql.Date exp) {
+		this.exp = exp;
+	}
+
+	@Override
+	public String toString() {
+		return "Date [dateId=" + dateId + ", memberId=" + memberId + ", proId=" + proId + ", batchCode=" + batchCode
+				+ ", mfd=" + mfd + ", exp=" + exp + "]";
+	}
+
+}
---
+++ src/main/java/tw/com/softleader/eeit8600/product/web/DateController.java
@@ -0,0 +1,27 @@
+package tw.com.softleader.eeit8600.product.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import tw.com.softleader.eeit8600.product.service.DateService;
+
+@Controller
+@RequestMapping("/date")
+public class DateController {
+	@Autowired
+	private DateService service;
+
+	@RequestMapping("/list")
+	public String listPage() {
+		return "/date/dateList";
+	}
+	@RequestMapping("/add")
+	public String addPage(){
+		return "/date/dateAddPage";
+	}
+	@RequestMapping("/edit")
+	public String editPage(){
+		return "date/dateEditPage";
+	}
+}
