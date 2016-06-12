f2bc8108438cd1339c3ce16419bf32a0a1f006e8
Hsiang <lovejune413@hotmail.com>
2016-06-13 00:23:07 +0800

    pratices

--- src/main/java/tw/com/softleader/eeit8600/notebook/web/NotebookController.java
+++ src/main/java/tw/com/softleader/eeit8600/notebook/web/NotebookController.java
@@ -7,9 +7,11 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import tw.com.softleader.eeit8600.notebook.entity.Notebook;
@@ -130,6 +132,14 @@ public class NotebookController {
 			attr.addFlashAttribute("result", "Success");
 			
 			return "redirect:/notebooks/add";
+		}
+		
+		
+		@RequestMapping("/sayHello")
+		@ResponseBody
+		public Notebook sayHello(@RequestBody Notebook notebook){
+			
+			return notebook;
 		}
 		
 }
