1569ffacb72b768b5307eb7e4fc864c152b4964f
hsiang0413 <lovejune413@gmail.com>
2016-06-13 19:48:09 +0800

    change place

---
+++ src/main/java/tw/com/queautiful/product/service/BrandService.java
@@ -0,0 +1,36 @@
+package tw.com.queautiful.product.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tw.com.softleader.eeit8600.product.dao.BrandDao;
+import tw.com.softleader.eeit8600.product.entity.Brand;
+
+@Service
+public class BrandService {
+
+	@Autowired
+	private BrandDao brandDao;
+	
+	public Brand getById(Long brandId){
+		return brandDao.findOne(brandId);
+	}
+	
+	public List<Brand> getAll(){
+		return brandDao.findAll();	
+	}
+	
+	public void insert(Brand brand){
+		brandDao.save(brand);
+	}
+	
+	public void update(Brand brand){
+		brandDao.save(brand);
+	}
+	
+	public void delete(Long brandId){
+		brandDao.delete(brandId);
+	}
+}
---
+++ src/main/java/tw/com/queautiful/product/service/CategoryService.java
@@ -0,0 +1,37 @@
+package tw.com.queautiful.product.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tw.com.softleader.eeit8600.product.dao.CategoryDao;
+import tw.com.softleader.eeit8600.product.entity.Category;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryDao categoryDao;
+	
+	public Category getById(Long id){
+		return categoryDao.findOne(id);
+	}
+	
+	public List<Category> getAll(){
+	
+		return categoryDao.findAll();
+	}
+	
+	public void insert(Category category){
+		categoryDao.save(category);
+	}
+	
+	public void update(Category category){
+		categoryDao.save(category);
+	}
+	
+	public void delete(Long id){
+		categoryDao.delete(id);
+	}
+}
