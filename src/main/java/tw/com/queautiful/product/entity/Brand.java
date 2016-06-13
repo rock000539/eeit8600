5389ee68a7818d42635958a347bcd54cd6ccfba3
hsiang0413 <lovejune413@gmail.com>
2016-06-13 19:47:45 +0800

    change place

---
+++ src/main/java/tw/com/queautiful/product/entity/Brand.java
@@ -0,0 +1,95 @@
+package tw.com.queautiful.product.entity;
+
+import java.sql.Blob;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "BRAND")
+public class Brand {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "BRANDID")
+	private Long brandId;
+
+	@Column(name = "BRANDNAME", length = 50)
+	private String brandName;
+
+	@Column(name = "BRANDIMG", length = 200)
+	private String brandImg;
+
+	@Column(name = "WEBSITE", length = 200)
+	private String website;
+
+	@Column(name = "BCFUNC", length = 20)
+	private String bcFunc;
+
+	@Column(name = "BRANDSHOW")
+	private boolean brandShow;
+
+	public Brand() {
+	}
+
+	public Brand(String brandName, String brandImg, String website, String bcFunc, boolean brandShow) {
+		this.brandName = brandName;
+		this.brandImg = brandImg;
+		this.website = website;
+		this.bcFunc = bcFunc;
+		this.brandShow = brandShow;
+	}
+
+	public Long getBrandId() {
+		return brandId;
+	}
+
+	public void setBrandId(Long brandId) {
+		this.brandId = brandId;
+	}
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public String getBrandImg() {
+		return brandImg;
+	}
+
+	public void setBrandImg(String brandImg) {
+		this.brandImg = brandImg;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public String getBcFunc() {
+		return bcFunc;
+	}
+
+	public void setBcFunc(String bcFunc) {
+		this.bcFunc = bcFunc;
+	}
+
+	public boolean isBrandShow() {
+		return brandShow;
+	}
+
+	public void setBrandShow(boolean brandShow) {
+		this.brandShow = brandShow;
+	}
+
+}
---
+++ src/main/java/tw/com/queautiful/product/entity/Category.java
@@ -0,0 +1,58 @@
+package tw.com.queautiful.product.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CATEGORY")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="CATEGORYID")
+	private Long categoryId;
+	
+	@Column(name="CATEGORYKIND",length=20)
+	private String categoryKind;
+	
+	@Column(name="CATEGORYLEVEL")
+	private Double categoryLevel;
+	
+	public Category(){}
+	
+	public Category(String categoryKind,Double categoryLevel){
+		this.categoryKind=categoryKind;
+		this.categoryLevel=categoryLevel;
+	}
+	
+	
+	public Long getId() {
+		return categoryId;
+	}
+	public void setId(Long id) {
+		this.categoryId = id;
+	}
+	public String getKind() {
+		return categoryKind;
+	}
+	public void setKind(String kind) {
+		this.categoryKind = kind;
+	}
+	public Double getLevel() {
+		return categoryLevel;
+	}
+	public void setLevel(Double level) {
+		this.categoryLevel = level;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + categoryId + ", kind=" + categoryKind + ", level=" + categoryLevel
+				+ "]";
+	}
+
+}
