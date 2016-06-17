54b4e706629a5431970b5b31db7714cb46ce23b7
rock000539 <Student@III-ITC>
2016-06-17 20:24:39 +0800

    fix

--- src/main/java/tw/com/queautiful/product/dao/ExpDateDao.java
+++
@@ -1,9 +0,0 @@
-package tw.com.queautiful.product.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tw.com.queautiful.product.entity.ExpDate;
-
-public interface ExpDateDao extends JpaRepository<ExpDate, Long>{
-
-}
