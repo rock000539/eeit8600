37bf358f9b6731a19301540243f57a31ac32678f
ChiHao0913 <purplemaple0913@gmail.com>
2016-07-28 16:15:32 +0800

    Spring FileUploadController for ckEditor

--- src/main/java/tw/com/queautiful/product/web/FileUploadController.java
+++ src/main/java/tw/com/queautiful/product/web/FileUploadController.java
@@ -6,6 +6,7 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -13,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,49 +30,61 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/upload")
 public class FileUploadController {
 	
+	// 查看路徑和檔案名稱用
 	private Logger log = LoggerFactory.getLogger(FileUploadController.class);
 
-	public static final String ROOT = "upload-dir";
-
+	// 路徑, 也可寫本地路徑  ex -> E:\Pic\
+	public static final String ROOT = "src\\main\\webapp\\WEB-INF\\img\\";
+	
+	// 讀取資源檔案都可使用  ex -> 圖片、檔案、property檔, 透過下面建構子Autowired建立
 	private final ResourceLoader resourceLoader;
 
 	@Autowired
 	public FileUploadController(ResourceLoader resourceLoader) {
 		this.resourceLoader = resourceLoader;
 	}
-
-	@RequestMapping(method = RequestMethod.GET, value = "/")
-	public String provideUploadInfo(Model model) throws IOException {
+	
+	// 讀取此路徑下所有檔案路徑轉為List集合
+	@RequestMapping(method = RequestMethod.GET, value = "/links")
+	@ResponseBody
+	public List<Link> getFileInfo(Model model) throws IOException {
 		
-		model.addAttribute("files", Files.walk(Paths.get(ROOT))
-				.filter(path -> !path.equals(Paths.get(ROOT)))
-				.map(path -> Paths.get(ROOT).relativize(path))
-				.map(path -> linkTo(methodOn(FileUploadController.class).getFile(path.toString())).withRel(path.toString()))
-				.collect(Collectors.toList()));
-
-		return "uploadForm";
+		List<Link> links = Files.walk(Paths.get(ROOT))
+		.filter(path -> !path.equals(Paths.get(ROOT)))
+		.map(path -> Paths.get(ROOT).relativize(path))
+		.map(path -> linkTo(methodOn(FileUploadController.class).getFile(path.toString())).withRel(path.toString()))
+		.collect(Collectors.toList());
+		
+		for(Link link : links) {
+			log.debug("link = {}", link);
+		}
+		
+		return links;
 	}
-
+	
+	// 透過檔名和檔案取得
 	@RequestMapping(method = RequestMethod.GET, value = "/{filename:.+}")
 	@ResponseBody
 	public ResponseEntity<?> getFile(@PathVariable String filename) {
 
 		try {
+			// ResponseEntity和ResponseBody有些類似, 但更具有彈性, 請參考  http://goo.gl/Oa4TQ3
+			// resourceLoader.getResource() 透過路徑和檔名取得檔案送回
 			return ResponseEntity.ok(resourceLoader.getResource("file:" + Paths.get(ROOT, filename).toString()));
 		} catch (Exception e) {
 			return ResponseEntity.notFound().build();
 		}
 	}
-
-	@RequestMapping(method = RequestMethod.POST, value = "/")
+	
+	// 將檔案寫入資料夾中
+	@RequestMapping(method = RequestMethod.POST, value = "/file")
 	public String handleFileUpload(@RequestParam("file") MultipartFile file,
 								   RedirectAttributes redirectAttributes) {
-
+		
 		if (!file.isEmpty()) {
-			try {
+			try { // 將傳入的檔案copy到資料夾(目前使用傳入的檔名)
 				Files.copy(file.getInputStream(), Paths.get(ROOT, file.getOriginalFilename()));
-				redirectAttributes.addFlashAttribute("message",
-						"You successfully uploaded " + file.getOriginalFilename() + "!");
+				redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
 			} catch (IOException|RuntimeException e) {
 				redirectAttributes.addFlashAttribute("message", "Failued to upload " + file.getOriginalFilename() + " => " + e.getMessage());
 			}
@@ -78,7 +92,13 @@ public class FileUploadController {
 			redirectAttributes.addFlashAttribute("message", "Failed to upload " + file.getOriginalFilename() + " because it was empty");
 		}
 
-		return "redirect:/";
+		return "redirect:/upload/";
+	}
+	
+	// 上傳圖片測試網站
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String uploadFormPage(Model model) {
+		return "/product/uploadForm";
 	}
 	
 }
