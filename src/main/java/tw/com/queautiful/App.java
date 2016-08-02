6e5000af562f982ae2bed73a0ab5e8a8706fef78
rock000539 <rock000539@gmail.com>
2016-08-02 18:04:22 +0800

    app update

--- src/main/java/tw/com/queautiful/App.java
+++ src/main/java/tw/com/queautiful/App.java
@@ -7,22 +7,31 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import tw.com.queautiful.product.entity.Product;
 import tw.com.queautiful.product.service.ProductService;
 
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @ServletComponentScan
-@SpringBootApplication(
-		exclude = {
-				org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
-		}
-)
-public class App implements CommandLineRunner {
+@SpringBootApplication
+public class App extends SpringBootServletInitializer implements CommandLineRunner  {
 	
 	public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
     }
 
 	@Autowired
